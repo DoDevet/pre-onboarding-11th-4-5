bd95ed7ebea7e8bd5bc88053a7a7bf3213544b4f
Telescope <ehwlgns000@gmail.com>
2023-07-16 20:35:12 +0900

    feat: create initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+interface Keyword {
+  keyword: string;
+  search_data: ISearchData[];
+}
+
+interface ISearchData {
+  sickCd: string;
+  sickNm: string;
+}
 
 function App() {
+  const [searchData, setSearchData] = useState<Keyword[]>([
+    { keyword: "", search_data: [] },
+  ]);
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const getSearchParams = () => {
+      if (
+        input !== "" &&
+        searchData?.findIndex((prev) => prev.keyword === input) === -1
+      )
+        (async () => {
+          const data: ISearchData[] = await (
+            await fetch(`http://localhost:4000/sick?q=${input}`)
+          ).json();
+          console.log("calling api");
+          setSearchData((prev) => {
+            const searchList = prev ? [...prev] : [];
+            searchList?.push({
+              keyword: input,
+              search_data: data?.filter(
+                (prev) =>
+                  prev.sickNm[input.length - 1] === input[input.length - 1]
+              ),
+            });
+            return searchList;
+          });
+        })();
+    };
+    const interval = setInterval(getSearchParams, 150);
+    return () => clearInterval(interval);
+  }, [input, searchData]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="flex items-center flex-col min-h-screen space-y-3 max-w-sm mx-auto ">
+        <h1 className="py-20">Search Data</h1>
+        <form className="w-full">
+          <input
+            className="w-full px-2"
+            placeholder="Input"
+            onChange={(e) => setInput(e.currentTarget.value)}
+          />
+        </form>
+
+        <div className="bg-white p-2 w-full min-h-[48px] divide-y">
+          {input && (
+            <>
+              <p>검색어</p>
+              <div>{input}</div>
+            </>
+          )}
+
+          <div>
+            <p>추천 검색어</p>
+            {searchData?.map((data) =>
+              data.keyword === input
+                ? data.search_data.map((data) => (
+                    <div key={data.sickCd}>{data.sickNm}</div>
+                  ))
+                : ""
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
